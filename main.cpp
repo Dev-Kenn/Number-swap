a4300063f379b4e9c3f9327287dbc75e9e59ee60
Mark Kenneth Ferrer <81341766+Dev-Kenn@users.noreply.github.com>
2021-06-01 08:37:28 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	int x, y;
+	
+	cout <<"Number Swapper\n";
+	cout <<"First number: ";
+	cin >> x;
+	cout <<"Second number: ";
+	cin >> y;
+	
+	cout <<"\nSwapped: " <<y; cout <<" " <<x;
+	return 0;
+}
